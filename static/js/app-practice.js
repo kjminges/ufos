024784f090eef705eb4cf92a337a4e33873a2694
Kurt Minges <kurtminges@gmail.com>
2022-03-04 16:42:51 -0500

    Module 11 Challenge w/o README

--- static/js/app-practice.js
+++ static/js/app-practice.js
@@ -7,29 +7,29 @@ var tbody = d3.select("tbody");
 // Indicate to JavaScript to use an empty string when creating the table
 function buildTable(data) {
     tbody.html("");
+    
+    // Indicate to JavaScript to find the <tbody> tag within the HTML and add a table row
+    // Reference one object from the array of UFO sightings, go into the dataRow, specify one item at a time and each value from the variable to a table cell
+    data.forEach((dataRow) => {
+        let row = tbody.append("tr");
+        Object.values(dataRow).forEach((val) => {
+            let cell = row.append("td");
+            cell.text(val);
+            }
+        );
+    });
 }
-
-// Indicate to JavaScript to find the <tbody> tag within the HTML and add a table row
-// Reference one object from the array of UFO sightings, go into the dataRow, specify one item at a time and each value from the variable to a table cell
-data.forEach((dataRow) => {
-    let row = tbody.append("tr");
-    Object.values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);
-        }
-    );
-})
 
 // Indicate through D3 to look for where date values are stored on the webpage and grab/store the information in the "date" variable.
 // Set a default filter and save it as a new variable
 function handleClick() {
-    let data = d3.select("#datetime").property("value");
+    let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
     if (date) {
     
         //  Apply a filter method that will match the datetime value to the filtered date value (=== is a strict equality, == losse equality)
         filteredData = filteredData.filter(row => row.datetime === date);
-    };
+    }
     buildTable(filteredData);
 }
 
